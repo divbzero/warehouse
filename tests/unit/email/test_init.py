81e638c3f8debd924cd36009d65ee4dd6e40f4c2
Chris Lei <chris@divbzero.com>
2022-06-23 04:45:24 -0700

    Emails for manage team "Settings"

--- tests/unit/email/test_init.py
+++ tests/unit/email/test_init.py
@@ -2891,6 +2891,7 @@ class TestTeamEmails:
         ("email_template_name", "send_team_email"),
         [
             ("team-created", email.send_team_created_email),
+            ("team-deleted", email.send_team_deleted_email),
         ],
     )
     def test_send_team_email(
